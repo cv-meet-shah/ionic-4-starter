1fc63653918127bfdb74aaecc6526e4d73427ff1
Meet <meet.shah@clairvoyantsoft.com>
2019-07-19 20:58:57 +0530

    Usage of placeholder loading.

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { TabsPage } from './tabs.page';
+import { ComponentModule } from 'src/app/components/component.module';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentModule,
     RouterModule.forChild(routes)
   ],
   declarations: [TabsPage]
